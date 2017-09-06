941c3cbebe4e210519961ca658c8ee2fc235ec02
SearChen <kuanghouming@imeijian.cn>
2017-09-06 10:42:56 +0800

    颜色百分比

--- app/components/getImageColor/index.js
+++ app/components/getImageColor/index.js
@@ -15,7 +15,8 @@ class Index extends PureComponent {
         this.width = null;
         this.height = null;
         this.context = null;
-        this.colorArrays= [[],[],[],[],[],[]];
+        this.colorArrays = [[], [], [], [], []];
+        this.resetPoints = [];
     }
 
     state = {
@@ -23,19 +24,21 @@ class Index extends PureComponent {
         aloneStyle: null
     }
 
-    static propTypes = {
-    }
+    static propTypes = {}
+
     componentDidMount() {
     }
+
     componentWillUnMount() {
 
     }
+
     CanvasImage = (image) => {
         this.canvas = document.createElement('canvas');
-        document.body.appendChild(this.canvas);
+        // document.body.appendChild(this.canvas);
         this.context = this.canvas.getContext('2d');
-        this.width = this.canvas.width = image.width;
-        this.height = this.canvas.height = image.height;
+        this.width = this.canvas.width = image.naturalWidth;
+        this.height = this.canvas.height = image.naturalHeight;
         this.context.drawImage(image, 0, 0, this.width, this.height);
     }
     getPixelCount = () => {
@@ -51,28 +54,26 @@ class Index extends PureComponent {
         return {pixelCount, pixels}
     }
     getRGB = () => {
-        let palette, length = 0, test = false; //[1,9, 100];
+        let palette, length = 0, allValue = [], test = false;//[216,217, 211];
 
         if (test) {
             console.log(this.getHSV(test))
-        }else {
+        } else {
             palette = this.getPalette(this.img0);
             console.log(palette)
 
             console.time('----------- loop');
-            let pixelArray = [], {pixelCount, pixels} = palette, quality = 4;
-            for (var i = 0, offset, r, g, b, a; i <= pixelCount; i = i + quality) {
-                offset = i * 4;
+            let {pixelCount, pixels} = palette, quality = 4;
+            for (var i = 0, offset, r, g, b, a; i <= pixelCount * 4; i = i + quality) {
                 r = pixels[i + 0];
                 g = pixels[i + 1];
                 b = pixels[i + 2];
                 a = pixels[i + 3];
                 // If pixel is mostly opaque and not white
-                if (a >= 125) {
-                    if (!(r > 250 && g > 250 && b > 250)) {
+                if (a > 0) {
+                    if (!(r === 255 && g === 255 && b === 255)) {
                         let hsv = this.getHSV([r, g, b]);
-                        // this.readRange(hsv);
-                        pixelArray.push(hsv);
+                        this.readRange(hsv, pixels[i]);
 
                     }
                 }
@@ -80,60 +81,96 @@ class Index extends PureComponent {
             console.timeEnd('----------- loop')
 
             console.time('++++++++++++ loop');
-            for (let k = 0; k < pixelArray.length; k++) {
-                this.readRange(pixelArray[k]);
-            }
-            console.timeEnd('++++++++++++ loop')
-
-            this.colorArrays.map((v) => length += v.length)
+            this.colorArrays.map((v) => {
+                length += v.length;
+            })
 
             for (let l = 0; l < this.colorArrays.length; l++) {
                 let obj = this.getPercentage(this.colorArrays[l]),
-                    values = Object.values(obj), max = _max(values);
-                values.map((v, i) => max === v && console.log('分类：' + l + ' 下标： ' + i + ' 百分比：' + max / length + '  ----- max ' + max + '  length ' + length +
-                    ' 色值： ' + ColorValue[l][1][i]))
+                    keys = Object.keys(obj),
+                    values = Object.values(obj),
+                    max = _max(values);
+                allValue = allValue.concat(values);
             }
-            console.log(this.colorArrays)
+
+            this.getPercent(allValue, length);
+            // console.log(this.colorArrays)
+
+            console.timeEnd('++++++++++++ loop')               
         }
     }
-    getHSV = (f) => {
-        let max = _max(f), v = max, min = _min(f), s = (max - min)/max,
-            index = f.indexOf(max), h;
-       if (f[[0]] === f[1]){
-           index = 1;
-       } else if(f[0]===f[2]){
-           index = 2;
-       } else if(f[1] === f[2]){
-            index = 0;
-       }
-       switch (index){
-           case 0:
-               h = Math.round(60 * (f[1] - f[2]) / (max - min));
-               break;
-           case 1:
-               h = 120 + Math.round(60 * (f[2] - f[0]) / (max - min));
-               break;
-           case 2:
-               h = 240 + Math.round(60 * (f[0] - f[1]) / (max - min));
-               break;
-           default: break;
-       }
-       s = Math.floor(s * 255.0);
-       h = Math.round(h/2);
-       return {
-           h, s, v
-       }
+
+    getCompete = () => {
+        let c = document.getElementById('test'), canContext = c.getContext('2d');
+
+
+        canContext.drawImage(this.img0, 0, 0, this.width, this.height);
+
+        let imgData = canContext.getImageData(0, 0, 800, 800), data = imgData.data;
+        // resetArray = new Uint8Array([0,0,0,255, 0,0,0,255, 0,0,0,255, 0,0,0,255]),
+        // let imgData = canContext.createImageData(800, 800),  data = imgData.data;
+        // let data = imgData.data;
+
+        for (var j = 0; j <= data.length; j += 4) {
+            for (var k = 0; k < this.resetPoints.length; k++) {
+                if (data[j] === this.resetPoints[k]) {
+                    data[j] = 0;
+                    data[j + 1] = 0;
+                    data[j + 2] = 0;
+                    // data[j + 3] = 255;
+                }
+            }
+            // data[j]     = 0;     // red
+            // data[j + 1] = 0; // green
+            // data[j + 2] = 0; // blue
+
+        }
+        canContext.putImageData(imgData, 0, 0);
+
+        //
     }
-    readRange = (hsv) => {
+    getHSV = (f) => {
+        let max = _max(f), v = max, min = _min(f), s = (max - min) / max,
+            index = f.indexOf(max), h;
+        if (f[[0]] === f[1]) {
+            index = 1;
+        } else if (f[0] === f[2]) {
+            index = 2;
+        } else if (f[1] === f[2]) {
+            index = 0;
+        }
+        switch (index) {
+            case 0:
+                h = Math.round(60 * (f[1] - f[2]) / (max - min));
+                break;
+            case 1:
+                h = 120 + Math.round(60 * (f[2] - f[0]) / (max - min));
+                break;
+            case 2:
+                h = 240 + Math.round(60 * (f[0] - f[1]) / (max - min));
+                break;
+            default:
+                break;
+        }
+        s = Math.floor(s * 255.0);
+        h = Math.round(h / 2);
+        return {
+            h, s, v
+        }
+    }
+    readRange = (hsv, index) => {
         let {h, s, v} = hsv;
-        for (let i = 0; i < ColorValue.length; i++){
+        for (let i = 0; i < ColorValue.length; i++) {
             let values = ColorValue[i][0];
-            for ( let j = 0; j < values.length; j ++) {
-                if (values[j][0][0] <= h &&  h < values[j][0][1]){
-                    if(values[j][1][0] <= s &&  s < values[j][1][1]){
-                        if(values[j][2][0] <= v &&  v < values[j][2][1]){
+            for (let j = 0; j < values.length; j++) {
+                if (values[j][0][0] <= h && h <= values[j][0][1]) {
+                    if (values[j][1][0] <= s && s <= values[j][1][1]) {
+                        if (values[j][2][0] <= v && v <= values[j][2][1]) {
                             this.colorArrays[i].push(j);
-                             // console.log('h: ' + h +' s: '+ s + ' v: ' + v + ' value: '+values[j] + ' 当前分类：' + i +' 当前索引：' + j +' 数据值：' + ColorValue[i][1][j])
+                            // if(i===0 && j=== 7){
+                            //     this.resetPoints.push(index);
+                            // }
+                            // console.log('h: ' + h +' s: '+ s + ' v: ' + v + ' value: '+values[j] + ' 当前分类：' + i +' 当前索引：' + j +' 数据值：' + ColorValue[i][1][j])
                         }
                     }
                 }
@@ -143,19 +180,43 @@ class Index extends PureComponent {
     getPercentage = (array) => {
         let value = this.getRepeatNum(array);
         console.log(value)
-        return value;        
+        return value;
     }
     getRepeatNum = (array) => { //获取数量
         var map = {};
-        for(var i = 0; i < array.length; i++){
+        for (var i = 0; i < array.length; i++) {
             var ai = array[i];
-            if(!map[ai]){
+            if (!map[ai]) {
                 map[ai] = 1;
-            }else{
+            } else {
                 map[ai]++;
             }
         }
         return map;
+    }
+
+    getPercent = (allValue, length) => {
+        let sortValue = allValue.sort(function(a, b){ return a -b;});
+        sortValue.reverse();
+        for (let l = 0; l < this.colorArrays.length; l++) {
+            let obj = this.getPercentage(this.colorArrays[l]),
+                keys = Object.keys(obj),
+                values = Object.values(obj);
+
+           values.map((v, i) =>{
+                if(v === sortValue[0]){
+                    console.log('分类：' + l + ' 下标： ' + keys[i] + ' 百分比：' + sortValue[0] / length + '  ----- max ' + sortValue[0] + '  length ' + length +
+                                    ' 色值： ' + ColorValue[l][1][keys[i]])
+                } else if(v === sortValue[1]) {
+                    console.log('分类：' + l + ' 下标： ' + keys[i] + ' 百分比：' + sortValue[0] / length + '  ----- max ' + sortValue[0] + '  length ' + length +
+                                                        ' 色值： ' + ColorValue[l][1][keys[i]])
+                } else if(v === sortValue[2]){
+                    console.log('分类：' + l + ' 下标： ' + keys[i] + ' 百分比：' + sortValue[0] / length + '  ----- max ' + sortValue[0] + '  length ' + length +
+                                                        ' 色值： ' + ColorValue[l][1][keys[i]])
+                }
+           })
+
+        }
     }
 
     rendFile = () => {
@@ -174,19 +235,20 @@ class Index extends PureComponent {
         return (
             <div styleName="container">
                 <button type="button" onClick={() => this.rendFile()}>读文件</button>
-                <img src={`worker/01.jpg`} ref={(el) => this.img0 = el }/>
+                <img src={`worker/01.jpg`} ref={(el) => this.img0 = el}/>
                 <button type="button" onClick={() => this.getRGB()}> 提取</button>
+                <button type="button" onClick={() => this.getCompete()}>区别点</button>
                 主色：
                 {
-                     aloneStyle ?<span style={{backgroundColor: `rgb(${aloneStyle})` }}></span> : null
+                    aloneStyle ? <span style={{backgroundColor: `rgb(${aloneStyle})`}}></span> : null
                 }
                 辅助色：
                 {
                     smallStyle && smallStyle.map((color, i) => {
-                        return <span key={'color-' + i} style={{backgroundColor: `rgb(${color})` }}></span>
+                        return <span key={'color-' + i} style={{backgroundColor: `rgb(${color})`}}></span>
                     })
                 }
-                <div></div>
+                <canvas id="test" width="800" height="800"></canvas>
             </div>
         )
     }
